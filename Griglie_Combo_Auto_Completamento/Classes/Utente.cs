48166d91efd2956651b34b4668dd4187bfcebfc3
Luca Schiavon <luca.schiavon@alice.it>
2019-03-06 20:56:14 +0100

    andato avanti con table

--- Griglie_Combo_Auto_Completamento/Classes/Utente.cs
+++ Griglie_Combo_Auto_Completamento/Classes/Utente.cs
@@ -1,18 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Griglie_Combo_Auto_Completamento.Classes
 {
+    [MetadataType(typeof(UtenteMetadata))]
     public class Utente
     {
 
-        //public int id { get; set; }
+        public int id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailID { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
+    }
+
+    public class UtenteMetadata
+    {
+
+        public int id { get; set; }
+        [Required (AllowEmptyStrings =false,ErrorMessage="Plaese insrt first name")]
+        public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Plaese insrt last name")]
+        public string LastName { get; set; }
+      
     }
 }
--- Griglie_Combo_Auto_Completamento/Controllers/HomeController.cs
+++ Griglie_Combo_Auto_Completamento/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ namespace Griglie_Combo_Auto_Completamento.Controllers
 {
     public class HomeController : Controller
     {
+        List<Utente> LstUtente = new List<Utente>();
         public ActionResult Index()
         {
             return View();
@@ -36,27 +37,87 @@ namespace Griglie_Combo_Auto_Completamento.Controllers
             ObjUtente1.Country = "Italia";
             ObjUtente1.City = "Verona";
             ObjUtente1.EmailID = "mbaruba.it";
-           // ObjUtente1.id = 1;
+            ObjUtente1.id = 1;
             Utente ObjUtente2 = new Utente();
             ObjUtente2.FirstName = "Carlo";
             ObjUtente2.LastName = "Cattaneo";
             ObjUtente2.Country = "Italia";
             ObjUtente2.City = "Mantova";
             ObjUtente2.EmailID = "mb2aruba.it";
-           // ObjUtente2.id =2;
+            ObjUtente2.id = 2;
             Utente ObjUtente3 = new Utente();
             ObjUtente3.FirstName = "Cristian";
             ObjUtente3.LastName = "Piazente";
             ObjUtente3.Country = "Italia";
             ObjUtente3.City = "Catania";
             ObjUtente3.EmailID = "caruba.it";
-           // ObjUtente3.id = 3;/aaa
-            var LstUtente = new List<Utente>();
+            ObjUtente3.id = 3;
+
             LstUtente.Add(ObjUtente1);
             LstUtente.Add(ObjUtente2);
             LstUtente.Add(ObjUtente3);
             //return Json(new {  LstUtente }, JsonRequestBehavior.AllowGet);
             return Json(new { data = LstUtente }, JsonRequestBehavior.AllowGet);
+        }
+        [HttpGet]
+        public ActionResult Save(int id)
+        {
+            var v = LstUtente.Where(a => a.id == id).FirstOrDefault();
+            return View(v);
+        }
+        [HttpPost]
+        public ActionResult Save(Utente u)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                if (u.id > 0)
+                {
+                    //edit
+                    Utente v1 = LstUtente.Where(a => a.id == u.id).FirstOrDefault();
+                    v1.FirstName = u.FirstName;
+                    v1.FirstName = u.LastName;
+                    v1.FirstName = u.Country;
+                    v1.FirstName = u.City;
+                    v1.EmailID = u.EmailID;
+                    v1.id = LstUtente.Count + 1;
+                }
+                else
+                {
+                    //save
+                    LstUtente.Add(u);
+                }
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var v = LstUtente.Where(a => a.id == id).FirstOrDefault();
+            if (v != null)
+            {
+                return View(v);
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteUtente(int id)
+        {
+            bool status = false;
+            var v = LstUtente.Where(a => a.id == id).FirstOrDefault();
+            if (v != null)
+            {
+                LstUtente.Remove(v);
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+
         }
     }
 }
